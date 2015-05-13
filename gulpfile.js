707b23d251f94f671273a959764d707f44d3d609
Orland Ureta <orlando.ureta@gmail.com>
2015-05-13 21:07:42 +0800

    Added typedoc generation to the gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,48 @@
-var gulp = require('gulp');
-var typescript = require('gulp-tsc');
+/* globals require */
 
-gulp.task('default', ['compile-internal', 'compile-xui']);
+(function()
+{
+	'use strict';
+	
+	var gulp = require('gulp'),
+		typescript = require('gulp-tsc'),
+		typedoc = require('gulp-typedoc');
 
-gulp.task('compile-internal', function() {
-	return gulp.src('src/internal/_references.ts')
-		.pipe(typescript({
-			declaration : true,
-			outDir : 'dist/',
-			out : 'internal.js'
-		}))
-		.pipe(gulp.dest('dist/'));
-});
+	gulp.task('default', ['compile-internal', 'compile-xui']);
 
-gulp.task('compile-xui', ['compile-internal'], function() {
-	return gulp.src('src/_references.ts')
-		.pipe(typescript({
-			declaration : true,
-			outDir : 'dist/',
-			out : 'xui.js'
-		}))
-		.pipe(gulp.dest('dist/'));
-});
+	gulp.task('compile-internal', function() {
+		return gulp.src('src/internal/_references.ts')
+			.pipe(typescript({
+				declaration : true,
+				outDir : 'dist/',
+				out : 'internal.js'
+			}))
+			.pipe(gulp.dest('dist/'));
+	});
 
-gulp.task('watch', function() {
-	gulp.watch('src/**/*.ts', ['default']);
-});
+	gulp.task('compile-xui', ['compile-internal'], function() {
+		return gulp.src('src/_references.ts')
+			.pipe(typescript({
+				declaration : true,
+				outDir : 'dist/',
+				out : 'xui.js'
+			}))
+			.pipe(gulp.dest('dist/'));
+	});
+
+	gulp.task('generate-typedoc', ['compile-xui'], function() {
+		return gulp
+        	.src(['src/_references.ts'])
+        	.pipe(typedoc({ 
+	            mode: 'file', 
+	            out: './docs/', 
+	            name: 'XUI Plugin Framework', 
+	            target: 'ES5',
+	            includeDeclarations: true
+	        }));
+	});
+
+	gulp.task('watch', function() {
+		gulp.watch('src/**/*.ts', ['default']);
+	});
+})();
