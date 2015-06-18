03636d1c276fb0165bc1b5b73a43db4803f99e64
Michael Ybanez <mikefyba@gmail.com>
2015-06-18 20:45:24 +0800

    timing fixes to searchItems() function, and remove old code version

--- src/core/view.ts
+++ src/core/view.ts
@@ -10,38 +10,6 @@ module xui.core {
         PREVIEW = 1
     }
 
-    function parseItems(items, iID, keyword, resolve) {
-        var matches: Array<Item> = [];
-
-        items.forEach((item, idx) => {
-            item.getID().then(id => {
-                if (Number(id) === iID) {
-                    matches.push(item);
-                }
-
-                return item.getName();
-            }).then(name => {
-                if (name.match(keyword) !== null) {
-                    matches.push(item);
-                } else {
-                    return item.getValue();
-                }
-
-                if (idx === items.length - 1) {
-                    resolve(matches);
-                }
-            }).then(val => {
-                if (val.value.match(keyword) !== null) {
-                    matches.push(item);
-                }
-
-                if (idx === items.length - 1) {
-                    resolve(matches);
-                }
-            });
-        });
-    };
-
     export class View {
 
         private id: number;
@@ -111,81 +79,71 @@ module xui.core {
             });
         }
 
-        searchItems(value: {}): Promise<Item[]> {
-            if (value['id'] === undefined && value['keyword'] === undefined) {
-                return;
-            }
-
-            var keyword: string = value['keyword'];
-            var iID: number = value['id'];
-            var pItems: Array<Item> = [];
-
-            // @TODO: Discuss if this is an 'ok' approach
-            return new Promise(resolve => {
-                this.getScenes().then(scenes => {
-                    scenes.forEach((scene, idx) => {
-                        scene.getItems().then(items => {
-                            if (items.length > 0) {
-                                pItems = pItems.concat(items);
-                            }
-
-                            // Valid scene is only until index 11
-                            if (idx === scenes.length - 1) {
-                                parseItems(pItems, keyword, iID, resolve);
-                            }
-                        });
-                    });
-                });
-            });
-        }
-
-        searchItems2(key: string): Promise<Item[]> {
+        searchItems(key: string): Promise<Item[]> {
             // detect if UUID or keyword
             let isID: boolean = /^{[A-F0-9-]*}$/i.test(key);
             let matches: Item[] = [];
 
             if (isID) {
+                let found = false;
                 return new Promise(resolve => {
                     this.getScenes().then(scenes => {
-                        scenes.forEach(scene => {
-                            scene.getItems().then(items => {
-                                items.some(item => { // assume ID is unique
-                                    if (item['id'] === key) {
-                                        matches.push(item);
+                        scenes.forEach((scene, idx, arr) => {
+                            if (!found) {
+                                scene.getItems().then(items => {
+                                    found = items.some(item => { // unique ID
+                                        if (item['id'] === key) {
+                                            matches.push(item);
+                                            resolve(matches);
+                                            return true;
+                                        } else {
+                                            return false;
+                                        }
+                                    });
+                                    if (idx === arr.length - 1) {
                                         resolve(matches);
-                                        return true;
-                                    } else {
-                                        return false;
                                     }
                                 });
-                            });
+                            }
                         });
                     });
-                    resolve(matches);
                 });
             } else {
                 return new Promise(resolve => {
                     this.getScenes().then(scenes => {
-                        scenes.forEach(scene => {
-                            scene.getItems().then(items => {
-                                items.forEach((item, idx) => { 
-                                    item.getName().then(name => {
-                                        if (name.match(key)) {
-                                            matches.push(item);
-                                            return '';
-                                        } else {
-                                            return item.getValue();
-                                        }
-                                    }).then(value => {
-                                        if (value.toString().match(key)) {
-                                            matches.push(item);
-                                        }
-                                    });
+                        return Promise.all(scenes.map(scene => {
+                            return new Promise(resolveScene => {
+                                scene.getItems().then(items => {
+                                    if (items.length === 0) {
+                                        resolveScene();
+                                    } else {
+                                        return Promise.all(items.map(item => {
+                                            return new Promise(resolveItem => {
+                                                item.getName().then(name => {
+                                                    if (name.match(key)) {
+                                                        matches.push(item);
+                                                        return '';
+                                                    } else {
+                                                        return item.getValue();
+                                                    }
+                                                }).then(value => {
+                                                    if (value.toString().match(
+                                                        key)) {
+                                                        matches.push(item);
+                                                    }
+                                                    resolveItem();
+                                                });
+                                            });
+                                        })).then(() => {
+                                            resolveScene();
+                                        });
+                                    }
                                 });
                             });
+                        })).then(() => {
+                            resolve(matches);
                         });
                     });
-                    resolve(matches);
                 });
             }
         }
