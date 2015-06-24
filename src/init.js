ffadf8a6013ed0c7577b5c478ac72c90739f134e
Michael Ybanez <mikefyba@gmail.com>
2015-06-24 19:48:36 +0800

    restructure initialization functionality as promises emit xui-ready event on document

--- src/init.js
+++ src/init.js
@@ -1,26 +1,47 @@
+/* globals xui, internal, di */
 (function() {
     'use strict';
 
-    window.addEventListener("load", function() {
+    var handleDependencyRegistration = function() {
+        return new Promise(function(done) {
+            window.addEventListener('load', function() {
 
-        // scope into xui namespace
-        xui.DependencyContainer = di;
-        delete window.di;
-        // base class dependencies
+                // scope into xui namespace
+                xui.DependencyContainer = di;
+                delete window.di;
+                // base class dependencies
 
-        xui.DependencyContainer
-            .register('app')
-                .as(xui.core.App)
-                .asSingleton()
-            .register('currentItem')
-                .as(xui.core.Item.getCurrentSource)
-            .register('currentScene')
-                .as(xui.core.Scene.get)
-            .register('mainView')
-                .instance(xui.core.View.MAIN)
-            .register('sourceWindow')
-                .as(xui.source.SourceWindow.getInstance)
-            .register('configWindow')
-                .as(xui.config.ConfigWindow.getInstance);
-    });
+                xui.DependencyContainer
+                    .register('app')
+                        .as(xui.core.App)
+                        .asSingleton()
+                    .register('currentItem')
+                        .as(xui.core.Item.getCurrentSource)
+                    .register('currentScene')
+                        .as(xui.core.Scene.get)
+                    .register('mainView')
+                        .instance(xui.core.View.MAIN)
+                    .register('sourceWindow')
+                        .as(xui.source.SourceWindow.getInstance)
+                    .register('configWindow')
+                        .as(xui.config.ConfigWindow.getInstance);
+
+                done();
+            });
+        });
+    };
+
+    var init = function() {
+        var p = internal.initPromises;
+        p.push(handleDependencyRegistration());
+
+        Promise.all(p).then(function() {
+            document.dispatchEvent(new CustomEvent('xui-ready', {
+                bubbles : 'true'
+            }));
+        });
+    };
+
+    init();
+
 }());
--- src/internal/init.ts
+++ src/internal/init.ts
@@ -6,45 +6,64 @@ module internal {
         window['internal'].persistConfig = config;
     }
 
-    export function init(): void {
-        // only valid for source plugin
-        if (Environment.isSourceHtml()) {
-            // initialize config URL if necessary
-            internal.exec('GetLocalPropertyAsync', 'prop:BrowserConfiguration',
-                result => {
-                    var configObj = JSON.parse(decodeURIComponent(result));
-                    if (configObj === null) {
-                        configObj = {};
-                    }
-                    if (configObj.configUrl === undefined) {
-                        var metas = document.getElementsByTagName("meta");
-                        for (var i = metas.length - 1; i >= 0; i--) {
-                            if (metas[i].name === 'config-url') {
-                                configObj.configUrl = metas[i].content;
-                                internal.exec('SetBrowserProperty',
-                                    'Configuration',
-                                    JSON.stringify(configObj));
+    function readMetaConfigUrl(): Promise<any> {
+        return new Promise(resolve => {
+            if (Environment.isSourceHtml()) {
+                // initialize config URL if necessary
+                internal.exec('GetLocalPropertyAsync',
+                    'prop:BrowserConfiguration',
+                    result => {
+                        var configObj = JSON.parse(decodeURIComponent(result));
+                        if (configObj === null) {
+                            configObj = {};
+                        }
+                        if (configObj.configUrl === undefined) {
+                            var metas = document.getElementsByTagName("meta");
+                            for (var i = metas.length - 1; i >= 0; i--) {
+                                if (metas[i].name === 'config-url') {
+                                    configObj.configUrl = metas[i].content;
+                                    internal.exec('SetBrowserProperty',
+                                        'Configuration',
+                                        JSON.stringify(configObj));
 
-                                persistConfig(configObj);
-                                break;
+                                    persistConfig(configObj);
+                                    break;
+                                }
                             }
                         }
-                    }
-                });
-        }
+                        resolve();
+                    });
+            } else {
+                resolve();
+            }
+        });
+    }
 
-        if (Environment.isSourceHtml() || Environment.isSourceConfig()) {
-            // initialize Item.getSource() functions
-            internal.exec('GetLocalPropertyAsync', 'prop:id',
-                result => {
-                    let id = decodeURIComponent(result);
-                    internal.Item.setBaseID(id);
+    function getCurrentSourceID(): Promise<any> {
+        return new Promise(resolve => {
+            if (Environment.isSourceHtml() || Environment.isSourceConfig()) {
+                // initialize Item.getSource() functions
+                internal.exec('GetLocalPropertyAsync', 'prop:id',
+                    result => {
+                        let id = decodeURIComponent(result);
+                        internal.Item.setBaseID(id);
 
-                    if (Environment.isSourceHtml()) {
-                        internal.Item.lockSourceSlot(id);
-                    }
-                });
-        }
+                        if (Environment.isSourceHtml()) {
+                            internal.Item.lockSourceSlot(id);
+                        }
+
+                        resolve();
+                    });
+            } else {
+                resolve();
+            }
+        });
+    }
+
+    export function init(): void {
+        let x = internal['initPromises'] = [];
+        x.push(readMetaConfigUrl());
+        x.push(getCurrentSourceID());
     }
 }
 
