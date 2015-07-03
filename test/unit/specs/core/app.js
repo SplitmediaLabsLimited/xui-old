dda9c8e9860d2f647ef8e4c8084a841de467f388
dcefram <me@danielcefram.com>
2015-07-03 11:57:02 +0800

    Create unit test case for fetching active stream controls (for commit#01baf23)

--- test/unit/specs/core/app.js
+++ test/unit/specs/core/app.js
@@ -1,4 +1,4 @@
-/* globals describe, it, expect, xui */
+/* globals describe, it, expect, xui, xdescribe */
 
 describe('xui.core.App', function() {
     'use strict';
@@ -38,55 +38,70 @@ describe('xui.core.App', function() {
         });
     });
 
+    describe('should be able to get active stream channels', function() {
+        var promise = App.getActiveStreamChannels();
+
+        it('through a promise', function() {
+            expect(promise).toBeInstanceOf(Promise);
+        });
+
+        it('that returns an array of Channels', function(done) {
+            promise.then(function(channels) {
+                if (channels.length > 0) {
+                    expect(channels).eachToBeInstanceOf(xui.core.Channel);
+                }
+                done();
+            });
+        });
+    });
+
     // OFF this test case by default, we do not want to save, load, and
     // clear our presentation everytime we run the test right?
-    if (false) {
-        describe('should be able to load a presentation', function() {
-            var filePath = 
-                'C:\\Users\\dara\\Documents\\SplitmediaLabs\\default.BPres';
+    xdescribe('should be able to load a presentation', function() {
+        var filePath = 
+            'C:\\Users\\dara\\Documents\\SplitmediaLabs\\default.BPres';
 
-            it('by supplying a file path', function() {
-                App.load(filePath);
+        it('by supplying a file path', function() {
+            App.load(filePath);
 
-                // I damn not know how to test this since there won't be any
-                // indication if loading is success or it failed big time
-                // so I'll just test if filePath is a string or not LoL
-                expect(typeof filePath).toBe('string');
-            });
-
-            it('by supplying a presentation object', function(done) {
-                App.getCurrentPresentation().then(function(presentation) {
-                    App.load(presentation);
-
-                    expect(presentation).toBeInstanceOf(xui.core.Presentation);
-
-                    done();
-                });
-            });
+            // I damn not know how to test this since there won't be any
+            // indication if loading is success or it failed big time
+            // so I'll just test if filePath is a string or not LoL
+            expect(typeof filePath).toBe('string');
         });
 
-        describe('should be able to save a presentation', function() {
-            var filePath = 
-                'C:\\Users\\dara\\Documents\\SplitmediaLabs\\default.BPres';
+        it('by supplying a presentation object', function(done) {
+            App.getCurrentPresentation().then(function(presentation) {
+                App.load(presentation);
 
-            it('by supplying a file path', function() {
-                App.save(filePath);
+                expect(presentation).toBeInstanceOf(xui.core.Presentation);
 
-                // I damn not know how to test this since there won't be any
-                // indication if loading is success or it failed big time
-                // so I'll just test if filePath is a string or not LoL
-                expect(typeof filePath).toBe('string');
+                done();
             });
         });
+    });
 
-        describe('should be able to clear', function() {
-            it('the presentation', function() {
-                App.clearPresentation();
+    xdescribe('should be able to save a presentation', function() {
+        var filePath = 
+            'C:\\Users\\dara\\Documents\\SplitmediaLabs\\default.BPres';
 
-                // I damn not know how to test this since there won't be any
-                // indication if loading is success or it failed big time
-                expect(App.clearPresentation).toBeInstanceOf(Function);
-            });
+        it('by supplying a file path', function() {
+            App.save(filePath);
+
+            // I damn not know how to test this since there won't be any
+            // indication if loading is success or it failed big time
+            // so I'll just test if filePath is a string or not LoL
+            expect(typeof filePath).toBe('string');
         });
-    }
+    });
+
+    xdescribe('should be able to clear', function() {
+        it('the presentation', function() {
+            App.clearPresentation();
+
+            // I damn not know how to test this since there won't be any
+            // indication if loading is success or it failed big time
+            expect(App.clearPresentation).toBeInstanceOf(Function);
+        });
+    });
 });
