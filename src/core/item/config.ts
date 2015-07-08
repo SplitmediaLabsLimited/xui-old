83d07acde3b9f6b60caee954fe01c2a32435a7cf
Michael Ybanez <mikefyba@gmail.com>
2015-07-08 21:31:38 +0800

    fix: null as string issue with load config

--- src/core/item/config.ts
+++ src/core/item/config.ts
@@ -21,7 +21,7 @@ module xui.core {
                 let slot = iItem.attach(this.id, this.viewID);
 
                 iItem.get('prop:BrowserConfiguration', slot).then(config => {
-                    let configObj = config === null ? {} : JSON.parse(config);
+                    let configObj = config === 'null' ? {} : JSON.parse(config);
                     let persist = internal['persistedConfig'];
                     for (var key in persist) {
                         delete configObj[key];
